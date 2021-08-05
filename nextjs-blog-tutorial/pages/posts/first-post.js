f710130b26328bea0cb3c28083121761ee5a7c6d
u-Hoshi <yuta000707@yahoo.co.jp>
2021-08-05 22:22:05 +0900

    About css

--- nextjs-blog-tutorial/pages/posts/first-post.js
+++ nextjs-blog-tutorial/pages/posts/first-post.js
@@ -1,21 +1,29 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import Head from 'next/head';
+import Layout from '../../components/layout';
 
 export default function FirstPost() {
   return (
     <>
-      <h1>First Post</h1>
-      <h2>
-        <Link href='/'>
-          <a>Back to home</a>
-        </Link>
-      </h2>
-      <Image
-        src='/images/profile.jpeg'
-        height={144}
-        width={144}
-        alt='your name'
-      />
+      <Layout>
+        <Head>
+          <title>First Second</title>
+          foo
+        </Head>
+        hoge
+        <h2>
+          <Link href='/'>
+            <a>Back to home</a>
+          </Link>
+        </h2>
+        <Image
+          src='/images/profile.jpeg'
+          height={144}
+          width={144}
+          alt='your name'
+        />
+      </Layout>
     </>
   );
 }
